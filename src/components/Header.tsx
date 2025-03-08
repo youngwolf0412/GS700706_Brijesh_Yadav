fcfafb6bcb7ffa8f46d9fed99ce71b1c119fb658
Brijesh Yadav <kumar.brijesh0412@gmail.com>
2025-03-08 13:05:23 +0530

    logo added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,17 @@
 export const Header = () => {
   return (
-    <div className="flex justify-between align-middle h-[50px]">
-      <div>
-        {/* <img src={logo} alt="logo" /> */}
-        logo
+    <div className="flex justify-between items-center h-[70px] px-5 bg-white shadow-sm">
+      <div className="flex items-center">
+        <img
+          src="/Gsynergy Logo V2 Long Description.svg"
+          alt="Gsynergy Logo"
+          className="h-12"
+        />
       </div>
-      <h1 className="text-3xl">Data Viewer App</h1>
-      <div>Signup/Signin</div>
+      <h1 className="text-3xl font-semibold text-gray-700">Data Viewer App</h1>
+      <div className="text-gray-600 font-medium cursor-pointer hover:text-gray-800">
+        Signup/Signin
+      </div>
     </div>
   );
 };
